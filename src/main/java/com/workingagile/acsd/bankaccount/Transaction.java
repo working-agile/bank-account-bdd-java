773eff2e7b5176b6fa51067bb94bd573347761a7
Axel <axelberle@yahoo.de>
2024-12-22 19:31:15 -0300

    Test-drive Transaction class

--- src/main/java/com/workingagile/acsd/bankaccount/Transaction.java
+++ src/main/java/com/workingagile/acsd/bankaccount/Transaction.java
@@ -2,15 +2,24 @@ package com.workingagile.acsd.bankaccount;
 
 public class Transaction {
 
+    String value;
+    String type;
+
     public Transaction(String transactionType, String transactionValue) {
 
+        if (transactionType == null || transactionType != "deposit") {
+            throw new IllegalArgumentException("unknown transaction type");
+        }
+
+        value = transactionValue;
+        type = transactionType;
     }
 
     public String getValue() {
-        return null;
+        return value;
     }
 
     public String getType() {
-        return null;
+        return type;
     }
 }
--- src/test/java/com/workingagile/acsd/bankaccount/TransactionTest.java
+++ src/test/java/com/workingagile/acsd/bankaccount/TransactionTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class TransactionTest {
 
@@ -17,16 +18,24 @@ public class TransactionTest {
 
     }
 
+    @Test
+    public void should_return_type_of_transaction() {
 
+        Transaction depositTransaction = new Transaction("deposit", "100");
 
+        assertThat(depositTransaction.getType(), is(equalTo("deposit")));
 
+    }
 
+    @Test
+    public void should_refuse_unknown_transaction_type() {
 
+        try {
+            new Transaction("unknown", "100");
+            fail("should not accept unknown transaction types");
+        } catch(IllegalArgumentException e) {}
 
-    // assertThat(depositTransaction.getType(), is(equalTo("deposit")));
-
-
-
+    }
 
 
 
