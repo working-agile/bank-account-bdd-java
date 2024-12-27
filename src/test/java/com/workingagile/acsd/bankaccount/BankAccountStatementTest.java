39190510776d22b439773f1cc645106147a93064
Axel <axelberle@yahoo.de>
2024-12-27 09:23:49 -0300

    TDD principle of triangulation

--- src/test/java/com/workingagile/acsd/bankaccount/BankAccountStatementTest.java
+++ src/test/java/com/workingagile/acsd/bankaccount/BankAccountStatementTest.java
@@ -1,8 +1,32 @@
 package com.workingagile.acsd.bankaccount;
 
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class BankAccountStatementTest {
 
-    
+    @Test
+    public void empty_statement_initial_balance_1000() {
+
+        // Arrange
+        BankAccount myBankAccount = new BankAccount(1000);
+
+        // Act
+        String actualStatement = myBankAccount.getStatement();
+
+        // Assert
+        String expectedStatement;
+        expectedStatement =
+                "  balance=1000\n" +
+                "  number of transactions=0\n" +
+                "  number of deposits=0\n" +
+                "  number of withdrawals=0\n" +
+                "---\n";
+                assertThat(actualStatement, is(equalTo(expectedStatement)));
+    }
 
 
 
