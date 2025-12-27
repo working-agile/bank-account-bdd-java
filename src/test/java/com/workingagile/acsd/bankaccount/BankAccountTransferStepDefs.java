00bd350d295f054d7e741498e8230c7ffa2d06eb
Axel Wilhelm Berle <axelberle@yahoo.de>
2025-12-27 08:58:35 -0300

    updated Gherkin style

--- src/test/java/com/workingagile/acsd/bankaccount/BankAccountTransferStepDefs.java
+++ src/test/java/com/workingagile/acsd/bankaccount/BankAccountTransferStepDefs.java
@@ -14,16 +14,16 @@ public class BankAccountTransferStepDefs {
     int initialBalanceNathan;
     int initialBalanceSabrina;
 
-    @Given("Nathan has a bank account with {int}")
-    public void nathan_has_a_bank_account_with(Integer initialBalance) {
+    @Given("Nathan has a bank account with a balance of {int} USD")
+    public void nathan_has_a_bank_account_with_a_balance_of_usd(Integer initialBalance) {
 
         bankAccountNathan = new BankAccount(initialBalance);
         initialBalanceNathan = initialBalance;
 
     }
 
-    @Given("Sabrina has a bank account with {int}")
-    public void sabrina_has_a_bank_account_with(Integer initialBalance) {
+    @Given("Sabrina has a bank account with a balance of {int} USD")
+    public void sabrina_has_a_bank_account_with_a_balance_of_usd(Integer initialBalance) {
 
         bankAccountSabrina = new BankAccount(initialBalance);
         initialBalanceSabrina = initialBalance;
@@ -33,7 +33,7 @@ public class BankAccountTransferStepDefs {
     Exception exceptionWhenTransferring;
 
     @When("Nathan tries to transfers {int} to Sabrina")
-    @When("Nathan transfers {int} to Sabrina")
+    @When("Nathan transfers {int} USD to Sabrina")
     public void nathan_transfers_to_sabrina(Integer transferAmount) {
 
         try {
@@ -44,41 +44,40 @@ public class BankAccountTransferStepDefs {
 
     }
 
-    @Then("the transfer should be cancelled")
-    public void the_transfer_should_be_cancelled() {
+    @Then("the transfer should be rejected")
+    public void the_transfer_should_be_rejected() {
 
         assertThat(exceptionWhenTransferring, is(not(nullValue())));
         assertThat(exceptionWhenTransferring, is(instanceOf(BankAccount.InsufficientBalanceException.class)));
 
     }
 
-    @Then("Sabrina should have {int}")
-    public void sabrina_should_have(Integer expectedBalance) {
+    @Then("Sabrina should have a balance of {int} USD")
+    public void sabrina_should_have_a_balance_of_usd(Integer expectedBalance) {
 
         assertThat(bankAccountSabrina.getBalance(), is(equalTo(expectedBalance)));
 
     }
 
-    @Then("Nathan should have {int}")
-    public void nathan_should_have(Integer expectedBalance) {
+    @Then("Nathan should have a balance of {int} USD")
+    public void nathan_should_have_a_balance_of_usd(Integer expectedBalance) {
 
         assertThat(bankAccountNathan.getBalance(), is(equalTo(expectedBalance)));
 
     }
 
-    @Then("both should have the same amount in their accounts")
-    public void both_should_have_the_same_amount_in_their_accounts() {
+    @Then("no money should have been moved")
+    public void no_money_should_have_been_moved() {
 
         assertThat(bankAccountNathan.getBalance(), is(equalTo(initialBalanceNathan)));
         assertThat(bankAccountSabrina.getBalance(), is(equalTo(initialBalanceSabrina)));
 
     }
 
-
     int transferFee;
 
-    @Given("the bank is charging a transfer fee of {int}")
-    public void the_bank_is_charging_a_transfer_fee_of(Integer transferFee) {
+    @Given("the bank is charging a transfer fee of {int} USD")
+    public void the_bank_is_charging_a_transfer_fee_of_usd(Integer transferFee) {
 
         this.transferFee = transferFee;
 
