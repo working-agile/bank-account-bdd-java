6ab4fff20d74940f54023709e54c40f738c6aea6
Axel Wilhelm Berle <axelberle@yahoo.de>
2025-12-27 19:04:26 -0300

    migrate changes from previous branches

--- src/test/java/com/workingagile/acsd/bankaccount/BankAccountEmailStepDefs.java
+++ src/test/java/com/workingagile/acsd/bankaccount/BankAccountEmailStepDefs.java
@@ -1,35 +1,49 @@
 package com.workingagile.acsd.bankaccount;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 
-
 public class BankAccountEmailStepDefs {
 
-
     EmailSender mockEmailSender;
+    BankAccount bankAccount;
+    Integer initialBalance;
 
-    @When("a client overdrafts his account")
-    public void a_client_overdrafts_his_account() {
+    @Given("a customer account has a balance of {int} USD")
+    public void a_customer_account_has_a_balance_of_usd(Integer balance) {
 
+        initialBalance = balance;
         mockEmailSender = mock(EmailSender.class);
-
-        try {
-
-            BankAccount bankAccount = new BankAccount(1000, mockEmailSender);
-            bankAccount.withdraw(2000);
-            fail("Not expected to get here");
-
-        } catch (Exception e) {
-        }
+        bankAccount = new BankAccount(balance, mockEmailSender);
 
     }
 
-    @Then("an email alert is sent to the bank")
-    public void an_email_alert_is_sent_to_the_bank() {
+    @When("the customer tries to withdraw {int} USD")
+    public void the_customer_tries_to_withdraw_usd(Integer amountToWithdraw) {
+        try {
+
+            bankAccount.withdraw(amountToWithdraw);
+            fail("Not expected to get here");
+
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Then("the withdrawal should be rejected")
+    public void the_withdrawal_should_be_rejected() {
+
+        assertThat(bankAccount.getBalance(), is(initialBalance));
+
+    }
+
+    @Then("an email alert is sent to the bank administration")
+    public void an_email_alert_is_sent_to_the_bank_administration() {
 
         verify(mockEmailSender, times(1)).sendEmailToBank();
 
