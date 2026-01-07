84699f65bcaed193a2156c2c57942c2eeb395056
Axel Wilhelm Berle <axelberle@yahoo.de>
2026-01-07 13:18:13 -0300

    step definitions stubs generated

--- src/test/java/com/workingagile/acsd/bankaccount/BankAccountStepdefs.java
+++ src/test/java/com/workingagile/acsd/bankaccount/BankAccountStepdefs.java
@@ -23,10 +23,29 @@ public class BankAccountStepdefs {
         this.account.deposit(toMoney(amount));
     }
 
+    @Then("the balance of the account should remain {double} USD")
     @Then("the balance of the account should be {double} USD")
     public void theBalanceOfTheAccountShouldBeUSD(Double expected) {
         assertMoneyEquals(expected, account.getBalance());
     }
+
+    @When("the customer withdraws {double} USD from the account")
+    public void the_customer_withdraws_usd_from_the_account(Double double1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the withdrawal should be declined due to insufficient funds")
+    public void the_withdrawal_should_be_declined_due_to_insufficient_funds() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
+
+
 
     private static BigDecimal toMoney(Double value) {
         return BigDecimal.valueOf(value).setScale(2, RoundingMode.HALF_EVEN);
