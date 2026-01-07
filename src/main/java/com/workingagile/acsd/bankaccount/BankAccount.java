c0bfee4937dd095f92a334de64d001fdfe948609
Axel Wilhelm Berle <axelberle@yahoo.de>
2026-01-07 14:01:43 -0300

    remove the compiler errors

--- src/main/java/com/workingagile/acsd/bankaccount/BankAccount.java
+++ src/main/java/com/workingagile/acsd/bankaccount/BankAccount.java
@@ -22,6 +22,14 @@ public class BankAccount {
         return balance;
     }
 
+    /**
+     * Placeholder API for withdrawals. Intentionally left without behavior to satisfy compilation only.
+     * Functional implementation will be provided by TDD in a later step.
+     */
+    public void withdraw(BigDecimal amount) throws InsufficientFundsException {
+        // no-op: production behavior intentionally not implemented yet
+    }
+
     private static BigDecimal toMoney(BigDecimal value) {
         return value.setScale(2, RoundingMode.HALF_EVEN);
     }
---
+++ src/main/java/com/workingagile/acsd/bankaccount/InsufficientFundsException.java
@@ -0,0 +1,9 @@
+package com.workingagile.acsd.bankaccount;
+
+public class InsufficientFundsException extends RuntimeException {
+
+    public InsufficientFundsException() {
+        super();
+    }
+
+}
